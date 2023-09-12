421976dda426758884bbae4b98b9bee696dfee8b
_NachoCampo <111907706+NachoCampo@users.noreply.github.com>
2023-09-12 13:50:57 -0300

    Descomentar

--- Grupo Tiri/Clasificación Masiva de Tiri.sql
+++ Grupo Tiri/Clasificación Masiva de Tiri.sql
@@ -1,14 +1,14 @@
 /*Insertar la clasificación de Artículos en Tiribelli de forma masiva con respecto a la carpeta "Marca"*/
 
---BEGIN TRAN
---INSERT INTO STA11ITC (FILLER, CODE, IDFOLDER, CODEA, ROW_VERSION, ID_STA11)
---SELECT
---    '',
---    COD_ARTICU,
---    CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_MARCA)[1]', 'nvarchar(max)'),
---    COD_ARTICU,
---    NULL,
---    ID_STA11
---FROM Sta11
---WHERE CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_MARCA)[1]', 'nvarchar(max)') IS NOT NULL
---ROLLBACK
+BEGIN TRAN
+INSERT INTO STA11ITC (FILLER, CODE, IDFOLDER, CODEA, ROW_VERSION, ID_STA11)
+SELECT
+    '',
+    COD_ARTICU,
+    CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_MARCA)[1]', 'nvarchar(max)'),
+    COD_ARTICU,
+    NULL,
+    ID_STA11
+FROM Sta11
+WHERE CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_MARCA)[1]', 'nvarchar(max)') IS NOT NULL
+ROLLBACK
