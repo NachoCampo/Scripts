3a37ba973098762b908cb8830754a18b2b93e121
_NachoCampo <ignaciocampo19@gmail.com>
2023-10-23 16:52:03 -0300

    Update [CENTRAL] Saldos Centralizados de Clientes.sql

--- Grupo Tiri/[CENTRAL] Saldos Centralizados de Clientes.sql
+++ Grupo Tiri/[CENTRAL] Saldos Centralizados de Clientes.sql
@@ -1,3 +1,6 @@
+-- Cambiar Condición de Venta y Talonario para que pasen a Central
+--Update GVA12 set COND_VTA = '1', TALONARIO = '10'  where N_COMP like '9999999999%'
+
 -- CTE para calcular el saldo inicial para los clientes que tienen comprobantes especiales 'FAC' con números que comienzan por '999999999999%'
 WITH SaldoInicial AS (
     SELECT 
@@ -28,7 +31,7 @@ SELECT
     SUM(
         CASE 
             -- Si es una 'FAC', suma el importe, si es 'N/C' o 'REC', resta el importe. En otros casos, suma 0.
-            WHEN CTA02.T_COMP = 'FAC' THEN ISNULL(IMPORTE, 0)
+            WHEN CTA02.T_COMP = 'FAC'  THEN ISNULL(IMPORTE, 0)
             WHEN CTA02.T_COMP IN ('N/C', 'REC') THEN ISNULL(-1 * IMPORTE, 0)
             ELSE 0
         END
@@ -38,7 +41,7 @@ SELECT
         (SELECT SUM(ISNULL(IMPORTE, 0)) 
          FROM CTA02 AS Imputaciones 
          WHERE Imputaciones.T_COMP_IMPUTADO = 'FAC' 
-         AND Imputaciones.N_COMP_IMPUTADO = CTA02.N_COMP COLLATE Modern_Spanish_CI_AI
+         AND Imputaciones.N_COMP_IMPUTADO = CTA02.N_COMP collate Modern_Spanish_CI_AI
          AND Imputaciones.COD_CLIENT = CTA02.COD_CLIENT 
          AND Imputaciones.NRO_SUCURS = CTA02.NRO_SUCURS 
          AND Imputaciones.T_COMP = 'REC'
@@ -47,7 +50,7 @@ SELECT
     -- Suma el saldo inicial acumulado para este cliente y sucursal si existe en el CTE SaldoInicial
     SUM(
         CASE 
-            WHEN SaldoInicial.SaldoInicial IS NOT NULL THEN SaldoInicial.SaldoInicial
+            WHEN SaldoInicial.SaldoInicial IS NOT NULL AND N_COMP NOT LIKE '999999999999%' THEN SaldoInicial.SaldoInicial
             ELSE 0
         END
     ) OVER (PARTITION BY CTA02.COD_CLIENT, CTA02.NRO_SUCURS ORDER BY CTA02.FECHA_EMIS, CTA02.T_COMP, CTA02.N_COMP) AS [Saldo Mon. Corriente]
@@ -58,7 +61,7 @@ LEFT JOIN
     SaldoInicial ON CTA02.COD_CLIENT = SaldoInicial.COD_CLIENT AND CTA02.NRO_SUCURS = SaldoInicial.NRO_SUCURS
 -- Filtra los registros para excluir clientes ocasionales y estados 'ANU'
 WHERE 
-    CTA02.COD_CLIENT <> '000000' AND CTA02.ESTADO <> 'ANU'
+    CTA02.COD_CLIENT <> '000000' AND CTA02.ESTADO <> 'ANU' 
 -- Agrupa los resultados por ciertos campos para evitar duplicados
 GROUP BY 
     CTA02.T_COMP, CTA02.N_COMP, CTA02.FECHA_EMIS, CTA02.COD_CLIENT, CTA02.ESTADO, CTA02.NRO_SUCURS, SaldoInicial.SaldoInicial
