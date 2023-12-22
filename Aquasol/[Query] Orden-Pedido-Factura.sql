ffd03a3ce77eaf7b5589b8113ae939748fa12cb7
_NachoCampo <ignaciocampo19@gmail.com>
2023-12-22 11:54:16 -0300

    Query - Circuito Completo
    Se agrega el campo de NroReserva para verlo en el informe

--- Aquasol/[Query] Orden-Pedido-Factura.sql
+++ Aquasol/[Query] Orden-Pedido-Factura.sql
@@ -35,3 +35,42 @@ GROUP BY
 Set dateformat DMY
 Select * from CircuitoCompleto
 
+
+
+--Se hacen modificaciones agregando la Reserva.
+Alter View CircuitoCompleto As (    
+SELECT   
+ Npo.ORDER_NRO_TIENDA AS [Nro. de Orden],   
+ Npo.NUMERO_PEDIDO AS [Nro. Pedido],   
+A.NComp AS [Nro. Factura],  
+Gva12.FECHA_EMIS AS [Fecha Factura],  
+Format (NPO.FECHA_ORDEN, 'dd/MM/yyyy') AS [Fecha Orden],  
+NPD.FECHA_ENTREGA AS [Fecha de Ingreso al Parque],  
+LOWER(A.Email) AS [Dirección Correo],  
+CASE WHEN A.EmailSent = 1 THEN 'Se envió correctamente' ELSE 'No se envió' END AS [Se envió Correo QR/Reserva],  
+CASE WHEN GVA12.CANT_MAIL != 0 THEN 'Se envió correctamente' ELSE 'No se envió' END AS [Se envió Factura x Correo],
+CASE WHEN GVA12.ULT_MAIL = '1800-01-01 00:00:00.000' THEN 'Sin datos' ELSE GVA12.ULT_MAIL END AS [Hora de Envio Factura],
+R.Code AS NroReserva
+FROM   
+ Gva12   
+ JOIN [WSC_AQUASOL].[dbo].[Qrs] A ON A.NComp = Gva12.N_COMP COLLATE Modern_Spanish_CI_AI  
+ JOIN [WSC_AQUASOL].[dbo].[Reservas] R ON R.Id = A.ReservaId
+ JOIN Gva55 ON Gva55.N_COMP = Gva12.N_COMP AND Gva55.T_COMP = Gva12.T_COMP  
+ JOIN NEXO_PEDIDOS_ORDEN NPO ON NPO.NUMERO_PEDIDO = Gva55.NRO_PEDIDO COLLATE Modern_Spanish_CI_AI  
+ JOIN NEXO_PEDIDOS_DIRECCIONES NPD ON NPO.ID_NEXO_PEDIDOS_ORDEN = NPD.ID_NEXO_PEDIDOS_ORDEN  
+WHERE   
+ A.CreatedBy = 'Web'   
+ --AND CAST(CreatedOn AS DATE) = CAST(GETDATE() AS DATE)  
+GROUP BY   
+ Npo.ORDER_NRO_TIENDA,   
+ npo.NUMERO_PEDIDO,   
+ A.NComp,   
+ A.EmailSent,
+ Gva12.CANT_MAIL,
+ A.Email,  
+ NPO.FECHA_ORDEN,  
+ Gva12.FECHA_EMIS,  
+ NPD.FECHA_ENTREGA,
+ Gva12.ULT_MAIL,
+ R.Code
+ )
