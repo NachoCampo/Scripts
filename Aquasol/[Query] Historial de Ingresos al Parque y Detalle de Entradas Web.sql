bf40c21b952bc6ce0f2555ed2edcdc3dd74a0047
_NachoCampo <ignaciocampo19@gmail.com>
2023-12-28 13:44:40 -0300

    Create Query - DeltalleVentaEntradasWeb y HistorialDeIngresosParque
    Se crean dos querys nuevas apuntando a WSC_AQUASOL.

---
+++ Aquasol/[Query] Historial de Ingresos al Parque y Detalle de Entradas Web.sql
@@ -0,0 +1,21 @@
+CREATE view HistorialDeIngresosAlParque As (  
+SELECT   
+    CAST(1 AS INT) AS [Contador],   
+    Articulos.Code AS [Codigo de Entrada],   
+    Articulos.Description AS [Descripción de Entrada],   
+    CONVERT(DATETIME, CONVERT(VARCHAR, Entry, 23)) AS [Fecha de Ingreso],   
+    DeviceEntry AS [Molinete]   
+FROM   
+    [WSC_AQUASOL].[dbo].[Qrs]   
+    JOIN Articulos ON Qrs.ArticuloId = Articulos.Id   
+WHERE   
+    [Entry] IS NOT NULL  
+)
+
+
+Create View DetalleVentaEntradasWeb AS (
+Select A.Code AS [Codigo] , A.Description AS [Descripción], Q.ReservaId AS [Número de Reserva], CONVERT(DATETIME, CONVERT(VARCHAR, q.CreatedOn, 23)) AS [Fecha de Compra], CONVERT(DATETIME, CONVERT(VARCHAR, Entry, 23)) AS [Fecha de Ingreso]from Qrs Q
+	Join Articulos A on
+		A.Id = Q.ArticuloId
+where Q.CreatedBy = 'WEB'
+)
