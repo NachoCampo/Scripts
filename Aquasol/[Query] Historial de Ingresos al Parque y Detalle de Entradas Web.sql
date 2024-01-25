2aa943404c61a3aa2e75af2dda9012220a248267
_NachoCampo <ignaciocampo19@gmail.com>
2024-01-25 16:11:56 -0300

    [Query] Consultas
    Se hace una actualización de la consulta con los detalles.

--- Aquasol/[Query] Historial de Ingresos al Parque y Detalle de Entradas Web.sql
+++ Aquasol/[Query] Historial de Ingresos al Parque y Detalle de Entradas Web.sql
@@ -25,3 +25,22 @@ CONVERT(DATETIME, CONVERT(VARCHAR, Entry, 23)) AS [Fecha de Ingreso]from Qrs Q
 		A.Id = Q.ArticuloId
 where Q.CreatedBy = 'WEB'
 )
+
+--Muestra el detalle de los ingresos del día, siempre y cuando tengan QR de Ingreso y también la entrada sea posterior a las 8.15 de la mañana.
+SELECT 
+    A.[Code] AS Codigo,
+    A.[Description] AS Descripcion,
+    COUNT(A.[Code]) AS Ingresaron
+FROM [WSC_AQUASOL].[dbo].[Qrs] QR
+INNER JOIN [dbo].[Articulos] A ON QR.ArticuloId = A.Id
+WHERE 
+    CAST(QR.[Entry] AS DATE) = CAST(GETDATE() AS DATE)
+    AND QR.[Entry] IS NOT NULL
+    AND CAST(QR.[Entry] AS TIME) >= '08:15:00'
+GROUP BY A.Code, A.[Description];
+
+--Muestra en "Cantidad de Ingresos" del día, siempre y cuando tengan QR de Ingreso y también la entrada sea posterior a las 8.15 de la mañana.
+SELECT COUNT(1) AS CantDeIngresos FROM [WSC_AQUASOL].[dbo].[Qrs]
+WHERE CAST([Entry] AS DATE) = CAST(GETDATE() AS DATE)
+AND [Entry] IS NOT NULL
+AND CAST([Entry] AS TIME) >= '08:15:00'
