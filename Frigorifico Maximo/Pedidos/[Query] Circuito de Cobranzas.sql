fd9024618727d06753fd34bc1fe2168eb86281d8
_NachoCampo <ignaciocampo19@gmail.com>
2024-02-27 16:19:49 -0300

    Update [Query] CircuitoCobranzas
    Se agrega la columna "Estado de Cobro" con las leyendas: "Totalmente cobrada", "Parcialmente cobrada" y "Sin cobrar".

--- Frigorifico Maximo/Pedidos/[Query] Circuito de Cobranzas.sql
+++ Frigorifico Maximo/Pedidos/[Query] Circuito de Cobranzas.sql
@@ -1,37 +1,43 @@
 --La vista muestra el Circuito de Cobranzas donde tenemos "Nro. Pedido", "Nro. Factura", "Nro. Recibo", "Total Factura" y "Total Cobrado".
 CREATE VIEW CircuitoCobranzas AS (
 SELECT
-	GVA14.COD_CLIENT AS [Cod. Cliente], --Codigo de Cliente
-	GVA14.RAZON_SOCI AS [Razon Social], --Razón Social del Cliente
-    GVA21.FECHA_PEDI AS [Fecha Pedido], --Fecha del Pedido
-    GVA21.NRO_PEDIDO AS [Nro. Pedido], -- Número de Pedido
-    --GVA12.FECHA_EMIS AS [Fecha Factura], -- Fecha de la Factura
-    GVA12.T_COMP AS [Tipo Comprobante], --Tipo de Comprobante relacionado a la Fac.
-    GVA55.N_COMP AS [Nro. Factura], --Nro. de Factura.
-    --GVA07.FECHA_VTO AS [Fecha Vencimiento], --Fecha de Vencimiento.
-    GVA07.T_COMP_CAN AS [Tipo Recibo], --Código de Recibo.
-    GVA07.N_COMP_CAN AS [Nro. Recibo] ,--Número de Recibo.
-    SUM(GVA12.IMPORTE) AS [Total Factura], --Total de la Factura.
-	SUM(GVA07.IMPORT_CAN) AS [Total Cobrado], --Total cobrado contra la Factura.
-    GVA21.USUARIO_INGRESO AS [Usuario Ingreso], --Usuario de Ingreso del Pedido.
-    GVA21.TERMINAL_INGRESO AS [Terminal Ingreso] --Terminal de Ingreso del Pedido.
+    GVA14.COD_CLIENT AS [Cod. Cliente],
+    GVA14.RAZON_SOCI AS [Razon Social],
+    GVA21.FECHA_PEDI AS [Fecha Pedido],
+    GVA21.FECHA_ENTR AS [Fecha Entrega Pedido],
+    GVA21.NRO_PEDIDO AS [Nro. Pedido],
+    GVA12.FECHA_EMIS AS [Fecha Factura],
+    GVA12.T_COMP AS [Tipo Comprobante Factura],
+    GVA55.N_COMP AS [Nro. Factura],
+    --GVA07.FECHA_VTO AS [Fecha Vencimiento],
+    GVA07.T_COMP_CAN AS [Tipo Recibo],
+    GVA07.N_COMP_CAN AS [Nro. Recibo] ,
+    SUM(GVA12.IMPORTE) AS [Total Factura],
+    SUM(GVA07.IMPORT_CAN) AS [Total Cobrado],
+    GVA21.USUARIO_INGRESO AS [Usuario Ingreso],
+    GVA21.TERMINAL_INGRESO AS [Terminal Ingreso],
+    CASE 
+        WHEN SUM(GVA12.IMPORTE) = SUM(GVA07.IMPORT_CAN) THEN 'Totalmente Cobrada'
+        WHEN SUM(GVA07.IMPORT_CAN) IS NULL THEN 'Sin Cobrar'
+        ELSE 'Parcialmente Cobrada'
+    END AS [Estado de Cobro]
 FROM 
     GVA12
 LEFT JOIN 
-    GVA14 ON GVA14.COD_CLIENT = GVA12.COD_CLIENT --Relacion Maestro de Clientes VS Cliente del Comprobante.
+    GVA14 ON GVA14.COD_CLIENT = GVA12.COD_CLIENT 
 LEFT JOIN 
-    GVA07 ON GVA12.N_COMP = GVA07.N_COMP AND GVA12.T_COMP = GVA07.T_COMP --Relacion Comprobantes Cuenta Corriente VS Imputaciones.
+    GVA07 ON GVA12.N_COMP = GVA07.N_COMP AND GVA12.T_COMP = GVA07.T_COMP
 JOIN 
-    GVA55 ON GVA55.N_COMP = GVA12.N_COMP -- Relacion Pedido/Factura VS Comprobantes de Cuenta Corriente.
+    GVA55 ON GVA55.N_COMP = GVA12.N_COMP
 JOIN 
-    GVA21 ON GVA21.NRO_PEDIDO = GVA55.NRO_PEDIDO --Relacion Encabezado de Pedido VS Pedido/Factura
-
-WHERE GVA21.USUARIO_INGRESO LIKE 'PEDIDO%' AND GVA12.COND_VTA IN (2,3,4,5,6) --Solo se filtra a los Usuarios "Pedido%" donde queremos que traiga PEDIDO1, PEDIDO2, PEDIDO3, PEDIDO4 y PEDIDO5. También se filtra por Condición de Venta en Cuenta Corriente.
-
+    GVA21 ON GVA21.NRO_PEDIDO = GVA55.NRO_PEDIDO
+WHERE 
+    GVA21.USUARIO_INGRESO LIKE 'PEDIDO%' AND GVA12.COND_VTA IN (2,3,4,5,6)
 GROUP BY 
-	GVA14.COD_CLIENT,
-	GVA14.RAZON_SOCI,
+    GVA14.COD_CLIENT,
+    GVA14.RAZON_SOCI,
     GVA21.FECHA_PEDI,
+    GVA21.FECHA_ENTR,
     GVA21.NRO_PEDIDO,
     GVA12.FECHA_EMIS,
     GVA12.T_COMP,
@@ -40,7 +46,8 @@ GROUP BY
     GVA07.T_COMP_CAN,
     GVA07.N_COMP_CAN,
     GVA21.USUARIO_INGRESO,
-    GVA21.TERMINAL_INGRESO
+    GVA21.TERMINAL_INGRESO;
+
 )
 
 
